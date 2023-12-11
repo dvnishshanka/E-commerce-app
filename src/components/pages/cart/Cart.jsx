17360c4c330a1b292f051707e034ef1509638f80
Vidushani <108521900+dvnishshanka@users.noreply.github.com>
2023-12-11 15:23:25 +0100

    Add Cart checkout model if user is not sign in

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -4,55 +4,80 @@ import { PrimaryBtn } from '../../common/button';
 import { LABEL_GO_TO_CHECKOUT } from '../../../constants/AppConstants';
 import CartItemCard from '../../common/cartItemCard';
 import defaultTheme from './../../../theme/index';
+import { Modal } from 'antd';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
   const cartData = useSelector((state) => state?.cart);
+  const auth = useSelector((state) => state.auth);
 
-  const checkoutHandler = () => {};
+  const checkoutHandler = () => {
+    if (!auth) {
+      setShowModal(true);
+    }
+  };
 
   return (
     <>
-      {cartData?.totalQty === 0 && (
+      <Modal
+        title="Sign in Required"
+        open={showModal}
+        onOk={() => {
+          navigate('/sign-in');
+        }}
+        onCancel={() => {
+          navigate('/register');
+        }}
+        okText="Sign in"
+        cancelText="Register"
+      >
+        Please login to proceed with the checkout.
+      </Modal>
+      {cartData?.totalQty === 0 ? (
         <CartContainer>
           <h2>Your shopping cart is empty</h2>
         </CartContainer>
-      )}
-      {cartData?.totalQty > 0 && (
-        <CartContainer>
-          <CartItemsWrapper>
-            <h2>
-              Your cart ({cartData.totalQty}
-              {cartData.totalQty === 1 ? ' item' : ' items'})
-            </h2>
-            {cartData?.cartItems.map((item, index) => {
-              return <CartItemCard key={index} item={item}></CartItemCard>;
-            })}
-          </CartItemsWrapper>
-          <CartSummary>
-            <h2>Total</h2>
-            <SummaryLine>
-              <p>Sub Total</p>
-              <p>{`${Number(cartData.totalPrice).toFixed(2)} €`}</p>
-            </SummaryLine>
-            <SummaryLine>
-              <p>Delivery</p>
-              <p>{`${Number(5).toFixed(2)} €`}</p>
-            </SummaryLine>
-            <SummaryLine style={{ fontWeight: 'bold' }}>
-              <p>Total (VAT included)</p>
-              <p>{`${Number(cartData.totalPrice + 5).toFixed(2)} €`}</p>
-            </SummaryLine>
-            <PrimaryBtn
-              style={{
-                backgroundColor: defaultTheme.colors.yellow,
-                color: defaultTheme.colors.black,
-              }}
-              onClick={checkoutHandler}
-            >
-              {LABEL_GO_TO_CHECKOUT}
-            </PrimaryBtn>
-          </CartSummary>
-        </CartContainer>
+      ) : (
+        cartData?.totalQty > 0 && (
+          <CartContainer>
+            <CartItemsWrapper>
+              <h2>
+                Your cart ({cartData.totalQty}
+                {cartData.totalQty === 1 ? ' item' : ' items'})
+              </h2>
+              {cartData?.cartItems.map((item, index) => {
+                return <CartItemCard key={index} item={item}></CartItemCard>;
+              })}
+            </CartItemsWrapper>
+            <CartSummary>
+              <h2>Total</h2>
+              <SummaryLine>
+                <p>Sub Total</p>
+                <p>{`${Number(cartData.totalPrice).toFixed(2)} €`}</p>
+              </SummaryLine>
+              <SummaryLine>
+                <p>Delivery</p>
+                <p>{`${Number(5).toFixed(2)} €`}</p>
+              </SummaryLine>
+              <SummaryLine style={{ fontWeight: 'bold' }}>
+                <p>Total (VAT included)</p>
+                <p>{`${Number(cartData.totalPrice + 5).toFixed(2)} €`}</p>
+              </SummaryLine>
+              <PrimaryBtn
+                style={{
+                  backgroundColor: defaultTheme.colors.yellow,
+                  color: defaultTheme.colors.black,
+                }}
+                onClick={checkoutHandler}
+              >
+                {LABEL_GO_TO_CHECKOUT}
+              </PrimaryBtn>
+            </CartSummary>
+          </CartContainer>
+        )
       )}
     </>
   );
