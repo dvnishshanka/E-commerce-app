087af0d6f006a5076282616faebaca8abcbe8bb0
Vidushani <108521900+dvnishshanka@users.noreply.github.com>
2023-12-12 23:31:19 +0100

    Add .env file

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,17 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBUNqe2XPJnHwhKhuxJ2ybsn7JaIB45brM",
-  authDomain: "flexicart-89b0d.firebaseapp.com",
-  databaseURL:
-    "https://flexicart-89b0d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "flexicart-89b0d",
-  storageBucket: "flexicart-89b0d.appspot.com",
-  messagingSenderId: "339081955675",
-  appId: "1:339081955675:web:97084b386361ccc426b27e",
+  apiKey:  process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
--- src/components/pages/categoryPage/style.js
+++ src/components/pages/categoryPage/style.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ItemsWrapper = styled.div`
   display: grid;
-  gap: 20px 20px;
-  grid-template-columns: auto auto auto;
+  gap: 10px 10px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   justify-content: space-around;
 `;
